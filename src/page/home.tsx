ddaeb90195eeefa6ef60c7ed81502fbba83dcfb5
saeid-tavazani <saeidtavazani@gmail.com>
2024-06-12 12:44:58 +0330

    fix home page

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,10 +1,34 @@
-import { Button } from "@/components/ui/button";
-
+import Layout from "@/components/ui/layout";
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa6";
+import { FaPlayCircle } from "react-icons/fa";
 const Home = () => {
   return (
-    <div>
-      <Button>hi</Button>
-    </div>
+    <Layout className="h-screen flex items-center justify-center flex-col gap-7">
+      <h1 className="text-2xl font-bold">سلام من سعید توازانی هستم</h1>
+      <p className="text-base lg:w-1/2 text-center">
+        این پروژه یک پنل ادمین مدرن و کاربرپسند است که با تمرکز بر سادگی استفاده
+        و تجربه کاربری بهینه طراحی شده است. کاربران می‌توانند به راحتی داده‌ها
+        را مدیریت کرده و به اطلاعات مورد نیاز خود دسترسی پیدا کنند.
+      </p>
+      <div className="flex items-center gap-3">
+        <Link
+          className="bg-primary hover:bg-primary/90 px-3 py-2 rounded-md text-sm flex gap-2 items-center transition-all"
+          to="signin"
+        >
+          <FaPlayCircle size={25} />
+          پیش نمایش
+        </Link>
+        <a
+          target="_blank"
+          href="https://github.com/saeid-tavazani/admin-panel"
+          className="border border-input bg-background hover:bg-accent hover:text-accent-foreground flex gap-2 items-center px-4 py-2 rounded-md text-sm transition-all"
+        >
+          <FaGithub size={25} />
+          GitHub
+        </a>
+      </div>
+    </Layout>
   );
 };
 
