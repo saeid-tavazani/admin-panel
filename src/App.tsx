aca20de9f1a4e223345ddbcf562238860d602670
saeid-tavazani <saeidtavazani@gmail.com>
2024-06-13 21:01:12 +0330

    add UserProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { UserProvider } from "./context/userContext.tsx";
 
 import Home from "./page/home";
 import SignIn from "./page/signin";
@@ -7,10 +8,13 @@ import SignIn from "./page/signin";
 const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signin" element={<SignIn />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="signin" element={<SignIn />} />
+        </Routes>
+      </UserProvider>
     </ThemeProvider>
   );
 };
