c4439a398748320aef6277663a1bc5492d69ef28
saeid-tavazani <saeidtavazani@gmail.com>
2024-06-15 22:49:16 +0330

    prop type btn

--- src/components/ui/modeToggle.tsx
+++ src/components/ui/modeToggle.tsx
@@ -9,13 +9,23 @@ import {
 } from "./dropdownMenu";
 import { useTheme } from "../ThemeProvider";
 
-export function ModeToggle() {
+export function ModeToggle({
+  variant = "outline",
+}: {
+  variant?:
+    | "link"
+    | "ghost"
+    | "secondary"
+    | "outline"
+    | "destructive"
+    | "default";
+}) {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant={variant} size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
