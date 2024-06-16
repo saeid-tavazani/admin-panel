7e0d5faf757939ed1ced848fe4e5a0edba30ddc0
saeid-tavazani <saeidtavazani@gmail.com>
2024-06-16 18:23:57 +0330

    Add user authentication check to Dashboard component
    - Implement useEffect hook to check if a user is authenticated.
    - Redirect to the signin page if no user is found in the context.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="signin" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </UserProvider>
     </ThemeProvider>
--- src/page/dashboard/index.tsx
+++ src/page/dashboard/index.tsx
@@ -1,8 +1,18 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Aside from "@/components/dashboard/aside";
 import Header from "@/components/dashboard/header";
-import { Outlet } from "react-router-dom";
+import { useUserContext } from "@/context/userContext";
 
 const Dashboard = () => {
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/signin");
+    }
+  }, [user]);
   return (
     <section className="w-full h-screen flex">
       <Aside />
